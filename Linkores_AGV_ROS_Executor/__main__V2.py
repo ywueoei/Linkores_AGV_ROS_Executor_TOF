002d84f06fd970dfc9226ff8e89ea24a2d5543e7
YWPC-LN\ywueo <yue_ei@foxmail.com>
2022-07-21 15:27:22 +0800

    托盘识别，demo 2 :local pass

--- Linkores_AGV_ROS_Executor/__main__V2.py
+++ Linkores_AGV_ROS_Executor/__main__V2.py
@@ -12,7 +12,7 @@ import re
 import copy
 import AudioPlayer
 import global_var
-from can_bus import can0 
+from can_bus import can0
 from log import logger
 from locator import locator_protocol
 from scheduler_connect import scheduler_protocol
@@ -102,7 +102,7 @@ def get_fork_height(ad_value, fork_ad_list=config.fork_ad_list, fork_height_list
 def straight_or_turn(p1, p2):
     while p1.phi < 0:
         p1.phi += math.pi * 2
-    
+
     while p2.phi < 0:
         p2.phi += math.pi * 2
 
@@ -209,23 +209,28 @@ first_flag = True
 mpc_path = None
 def tray_identification(x,y,phi,):
     global first_flag, mpc_path
-    # beeline = -1.5
-    config.adjust_distance = 0
+    beeline_set = 2 + 1.5
+    config.adjust_distance = 0.7
     print("endPosition----->", x,",",y,",",phi/math.pi*180)
     if first_flag:
         x0 = x
         y0 = y
         # phi0 = phi/math.pi*180 +5
         first_flag = False
-        tray_position = tof_camera_data.rcv_tof
-        tray_position = [10.6442 , 3.3032 , -8.5256, 0]
+        # tray_position = tof_camera_data.rcv_tof
+        # tray_position = [11.4587 , 2.936 , -9.087, 0]
+        tray_position = [x0 , y0 , round(phi/math.pi*180,4), 0]
         dx = tray_position[0] - x0
         dy = tray_position[1] - y0
         phi0 = tray_position[2]
-        beeline = - (math.sqrt(dx*dx + dy*dy))
+        beeline = - (math.sqrt(dx**2 + dy**2))
 
-        if abs(beeline) > 0.001:
-            mpc_path = "100," + format(x0, ".4f") + "," + format(y0, ".4f") + "," + format(phi0, ".4f") + "," + format(beeline, ".4f") +  ",0.0, 0.0, 0.2, 6666, 6, 6," + format(config.adjust_distance, ".4f")
+        x_initial = tray_position[0] + beeline_set * math.cos(phi0/180*math.pi)
+        y_initial = tray_position[1] + beeline_set * math.sin(phi0/180*math.pi)
+        print("startPosition----->", x_initial, ",", y_initial, ",", phi0, tray_position)
+
+        if abs(beeline_set) > 0.001:
+            mpc_path = "100," + format(x_initial, ".4f") + "," + format(y_initial, ".4f") + "," + format(phi0, ".4f") + "," + format(-(beeline_set-1.5), ".4f") +  ",0.0, 0.0, 0.2, 6666, 6, 6, -1.5,0.0, 0.0, 0.2, 7777, 7, 7," + format(config.adjust_distance, ".4f")
         else:
             logger.info("Tray position is error")
             print("Tray position is error")
